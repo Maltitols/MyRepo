52d30da8d6e444b00d960e4310af2c54b32d471f
leechangho <maltitol7090@gmail.com>
2019-12-30 11:03:48 +0900

    급한작업

--- Git01_Java/src/test/main/MainClass01.java
+++ Git01_Java/src/test/main/MainClass01.java
@@ -8,5 +8,6 @@ public class MainClass01 {
 		System.out.println("lab1 branch에서 작업함2");
 		System.out.println("master branch hello~");
 		System.out.println("lab1 branch hello~");
+		System.out.println("급한작업");
 	}
 }
